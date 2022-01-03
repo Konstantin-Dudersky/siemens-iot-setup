fec25bec3b2423714f2a68b6608365df324e20bc
konstantin <konstantin.dudersky@gmail.com>
2022-01-03 21:44:58 +0300

    python and poetry install

--- setup.sh
+++ setup.sh
@@ -1,33 +1,39 @@
 #!/bin/bash
 
+PYTHON_VER=3.10.1
+
+echo
+echo "-----> Upgrading system:"
+apt -y update
+apt -y upgrade
+apt -y curl build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev
+apt -y install libssl-dev libsqlite3-dev libreadline-dev libffi-dev libbz2-dev liblzma-dev
+apt -y install python3-distutils
+
+echo
+echo "-----> Create temp folder:"
+cd ~ || exit
+mkdir temp
+cd temp || exit
+
 echo
 echo "-----> Existing python version:"
 python3 -V
 
 echo
-echo "-----> Updating system:"
-sudo apt update
-sudo apt -y upgrade
-
-echo
-echo "-----> Create temp folder"
-cd ~ || exit
-mkdir temp && cd temp || exit
-
 echo "-----> Install poetry:"
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
-poetry completions bash > /etc/bash_completion.d/poetry.bash-completion
+# shellcheck disable=SC1090
+source ~/.poetry/env
 poetry config virtualenvs.in-project true
 
 echo
 echo "-----> Install python 3.10:"
-sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev curl libbz2-dev liblzma-dev
-
-wget https://www.python.org/ftp/python/3.10.1/Python-3.10.1.tgz
-tar -xf Python-3.10.1.tgz && cd Python-3.10.1 || exit
-./configure --enable-optimizations && make -j 2 # 2 - число ядер, nproc
-sudo make altinstall
+wget https://www.python.org/ftp/python/$PYTHON_VER/Python-$PYTHON_VER.tgz
+tar -xf Python-$PYTHON_VER.tgz && cd Python-$PYTHON_VER || exit
+./configure --enable-optimizations && make -j "$(nproc)"
+make altinstall
 
 echo
-echo "-----> Install python version"
+echo "-----> Installed python version"
 python3.10 --version

006630132c16e1d0105817fedd2df000a0720d32
konstantin <konstantin.dudersky@gmail.com>
2022-01-12 13:54:31 +0300

    add script for basic version

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # siemens-iot-setup
 
 
+## IOT2050 basic
+
+Скачиваем последнюю версию
+
+    wget https://github.com/Konstantin-Dudersky/siemens-iot-setup/releases/latest/download/iot2050basic-setup.sh
+
+Делаем выполняемым и запускаем
+
+    chmod +x iot2050basic-setup.sh && ./iot2050basic-setup.sh
+
+
 ## IOT2050 advanced
 
 Скачиваем последнюю версию
---
+++ iot2050basic-setup.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+PYTHON_VER=3.10.1
+
+echo
+echo "-----> Upgrading system:"
+sudo apt -y update
+sudo apt -y upgrade
+
+echo ""
+
+echo
+echo "-----> Install software:"
+sudo apt -y install curl htop nano git
+
+echo
+echo "-----> Create temp folder:"
+cd ~ || exit
+mkdir inosat
+cd inosat || exit
+
+echo
+echo "-----> Existing python version:"
+python3 -V
+
+echo
+echo "-----> Install poetry:"
+sudo apt -y install python3-distutils
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
+# shellcheck disable=SC1090
+source ~/.poetry/env
+poetry config virtualenvs.in-project true
+
+echo
+echo "-----> Install python 3.10:"
+sudo apt -y install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libsqlite3-dev
+sudo apt -y install libssl-dev libsqlite3-dev libreadline-dev libffi-dev libbz2-dev liblzma-dev
+wget https://www.python.org/ftp/python/$PYTHON_VER/Python-$PYTHON_VER.tgz
+tar -xf Python-$PYTHON_VER.tgz && cd Python-$PYTHON_VER || exit
+./configure --enable-optimizations && make -j "$(nproc)"
+sudo make altinstall
+
+echo
+echo "-----> Installed python version"
+python3.10 --version
